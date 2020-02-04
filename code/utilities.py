5099d1c9e6d1ba15ccb30f902045dcbe9a6d94e0
RuiXia <xr2739401611@gmail.com>
2020-02-04 17:28:17 +0000

    add likelihood broadcasting for nonGaussian likelihood;

--- code/utilities.py
+++ code/utilities.py
@@ -3,6 +3,8 @@ import numpy as np
 import tensorflow as tf
 import gpflow
 from gpflow.config import default_float, default_jitter
+from gpflow.base import Module
+from gpflow.likelihoods import Gaussian
 
 gpflow.config.set_default_float(np.float64)
 gpflow.config.set_default_jitter(1e-6)
@@ -33,3 +35,66 @@ def reparameterise(mean, var, z, full_cov=False):
         z_SDN1 = tf.transpose(z, (0, 2, 1))[:, :, :, None]
         f = mean + tf.matmul(chol, z_SDN1)[:, :, :, 0]
         return tf.transpose(f, (0, 2, 1)) # [S,N,D]
+    
+class BroadcastingLikelihood(Module):
+    """
+    A wrapper for the likelihood to broadcast over the samples dimension. The Gaussian doesn't
+    need this, but for the others we can apply reshaping and tiling.
+    With this wrapper all likelihood functions behave correctly with inputs of shape S,N,D,
+    but with Y still of shape N,D
+    """
+    def __init__(self, likelihood,**kwargs):
+        super().__init__(**kwargs)
+        self.likelihood = likelihood
+
+        if isinstance(likelihood, Gaussian):
+            self.needs_broadcasting = False
+        else:
+            self.needs_broadcasting = True
+
+    def _broadcast(self, f, vars_SND, vars_ND):
+        if self.needs_broadcasting is False:
+            return f(vars_SND, [tf.expand_dims(v, 0) for v in vars_ND])
+
+        else:
+            S, N, D = [tf.shape(vars_SND[0])[i] for i in range(3)]
+            vars_tiled = [tf.tile(x[None, :, :], [S, 1, 1]) for x in vars_ND]
+
+            flattened_SND = [tf.reshape(x, [S*N, D]) for x in vars_SND]
+            flattened_tiled = [tf.reshape(x, [S*N, -1]) for x in vars_tiled]
+
+            flattened_result = f(flattened_SND, flattened_tiled)
+            if isinstance(flattened_result, tuple):
+                return [tf.reshape(x, [S, N, -1]) for x in flattened_result]
+            else:
+                return tf.reshape(flattened_result, [S, N, -1])
+
+    def variational_expectations(self, Fmu, Fvar, Y):
+        f = lambda vars_SND, vars_ND: self.likelihood.variational_expectations(vars_SND[0],
+                                                                                vars_SND[1],
+                                                                                vars_ND[0])
+        return self._broadcast(f,[Fmu, Fvar], [Y])
+
+    def log_prob(self, F, Y):
+        f = lambda vars_SND, vars_ND: self.likelihood.logp(vars_SND[0], vars_ND[0])
+        return self._broadcast(f, [F], [Y])
+
+    def conditional_mean(self, F):
+        f = lambda vars_SND, vars_ND: self.likelihood.conditional_mean(vars_SND[0])
+        return self._broadcast(f,[F], [])
+
+
+    def conditional_variance(self, F):
+        f = lambda vars_SND, vars_ND: self.likelihood.conditional_variance(vars_SND[0])
+        return self._broadcast(f,[F], [])
+
+    def predict_mean_and_var(self, Fmu, Fvar):
+        f = lambda vars_SND, vars_ND: self.likelihood.predict_mean_and_var(vars_SND[0],
+                                                                             vars_SND[1])
+        return self._broadcast(f,[Fmu, Fvar], [])
+
+    def predict_density(self, Fmu, Fvar, Y):
+        f = lambda vars_SND, vars_ND: self.likelihood.predict_density(vars_SND[0],
+                                                                       vars_SND[1],
+                                                                       vars_ND[0])
+        return self._broadcast(f,[Fmu, Fvar], [Y])
